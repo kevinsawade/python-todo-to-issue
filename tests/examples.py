11858e41da78ffde8b838089e5444b43759673c0
Kevin <kevin.sawade@uni-konstanz.de>
2021-07-08 23:05:40 +0200

    Added an examples.py

---
+++ tests/examples.py
@@ -0,0 +1,43 @@
+############
+# DISCLAIMER
+# AND
+# LICENSING
+# INFO
+###########
+"""Module level Docstring.
+
+Examples:
+    This is just a text. The example happens here::
+
+        >>> import main as main
+        >>> # This is a comment
+        >>> # TODO This is a todo.
+        >>> #  assignees: kevin
+        >>> #  labels: devel
+
+
+"""
+
+
+def unfinished_func():
+    """Function-level docstring.
+
+    More info about the function in an explanatory text.
+
+    """
+    # This is a simple in-line comment
+    # Function will be passed, but I will also add todos:
+    pass
+
+
+class useless_class:
+    """
+
+    """
+
+    def __init__(self):
+        pass
+
+    @property
+    def random(self):
+        return 2
