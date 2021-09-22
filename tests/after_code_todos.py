67b1e23240095bf70a603ffe9160a29fd3d07d64
Kevin Sawade <kevin.sawade@uni-konstanz.de>
2021-09-22 14:14:37 +0200

    Pushing a new test file with todos after code lines.

---
+++ tests/after_code_todos.py
@@ -0,0 +1,5 @@
+self.is_recursive = False  # TODO - will need to be a 2nd pass as creation of all files is not complete
+self.delete = True  # todo: - test it doesn't remove hyphenated words
+# TODO - error if not divisible by 4
+if self.is_extra:
+    return  # TODO - for now ignoring anything after the filename
